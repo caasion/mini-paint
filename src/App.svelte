8d526ad29595e2bc68fc75254d7767346a803a64
caasion <103465188+caasion@users.noreply.github.com>
2025-10-11 18:21:10 -0400

    [feat] Use Header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
-    import { get } from 'svelte/store';
+  import { get } from 'svelte/store';
   import Canvas from './components/Canvas.svelte'
   import Palette from './components/Palette.svelte';
   import PenSelector from './components/PenSelector.svelte';
-    import TextTool from './components/TextTool.svelte';
+  import TextTool from './components/TextTool.svelte';
   import ToolSelector from './components/ToolSelector.svelte';
-    import { currentTool } from './tools';
+  import { currentTool } from './tools';
+    import Header from './components/Header.svelte';
 </script>
 
 <main>
-  <h1>Mini Painter</h1>
-
-  <div class="card">
-    <ToolSelector />
-    <PenSelector />
-    <Palette />
-    {#if $currentTool == "Text"}
-      <TextTool />
-    {/if}
-    <Canvas />
-    
-  </div>
+  <Header />
+  <ToolSelector />
+  <PenSelector />
+  <Palette />
+  {#if $currentTool == "Text"}
+    <TextTool />
+  {/if}
+  <Canvas />
 </main>
 
 <style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+
+<h1 style="text-align:center; margin:16px 0 8px; line-height:1; font-family: 'Indie Flower', cursive;">
+  Mini Paint
+</h1>
+<hr style="margin:6px 0">
+<h3 style="display:inline-block; margin-right:30px; font-family: 'Indie Flower', cursive;">File</h3>
+<h3 style="display:inline-block; margin-right:30px; font-family: 'Indie Flower', cursive;">Save</h3>
+<h3 style="display:inline-block; margin-right:30px; font-family: 'Indie Flower', cursive;">View</h3>
+<h3 style="display:inline-block; font-family: 'Indie Flower', cursive;">Clear</h3>
+<img src="redo.jepg.png" alt="Redo" style="width:40px; height:auto; position: relative; left: 60px; top: 13px;">
+<img src="undo.jepg.webp" alt="Undo" style="width:40px; height:auto; position: relative; right: 35px; top: 13px;">
