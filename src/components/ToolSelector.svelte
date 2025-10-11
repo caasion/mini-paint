dcfb7513f3304f41bd47dcbc45d5a447790554d7
caasion <103465188+caasion@users.noreply.github.com>
2025-10-11 14:59:02 -0400

    [feat] Add Tool Selector

---
+++ src/components/ToolSelector.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { currentTool, setCurrentTool } from "../stores/palette";
+</script>
+
+<div style="display: flex">
+    <div onclick={() => setCurrentTool("Brush")}>
+        Brush!!!!
+    </div>
+    <div onclick={() => setCurrentTool("Bucket")}>
+        Bucket!!!!
+    </div>
+</div>
+
--- src/stores/palette.ts
+++ src/stores/palette.ts
@@ -1,7 +1,15 @@
 import { writable } from "svelte/store";
+import type { Tool } from "../types";
 
 export const currentColor = writable("#000000");
 
 export function setCurrentColor(color: string) {
-    currentColor.set("#7f2b8f");
+    currentColor.set(color);
+}
+
+export const currentTool = writable("");
+
+export function setCurrentTool(tool: Tool) {
+    currentTool.set(tool);
+    console.log("Set current tool:", tool)
 }
